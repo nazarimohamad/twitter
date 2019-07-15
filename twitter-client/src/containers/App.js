ecb6acfb02ce136ffb7e63c5398510a3b6157fae
Ryan Nazari <ryannazari21@gmail.com>
2019-07-15 21:44:42 +0300

    Add bootstrap CSS and styling the navbar component

--- twitter-client/src/containers/App.js
+++ twitter-client/src/containers/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { configureStore } from "../store";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
+import Navbar from "./Navbar"
 
 const store = configureStore();
 
 const App = () => (
   <Provider store={store}>
     <Router>
-      <div>hello word</div>
+      <div className="onboarding">
+        <Navbar />
+      </div>
     </Router>
   </Provider>
 )
